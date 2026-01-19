8aaccbc0479637d61924458c93d0c7bfaf9fd82c
theoz249 <tz66@kent.ac.uk>
2026-01-19 14:25:26 +0000

    pythag

--- partTwo.py
+++ partTwo.py
@@ -1,9 +1,12 @@
-import math  
+import math
 
 def main():
-#TO DO  
-
+#TO DO
+    A = int(input("length of A"))
+    B = int(input("length of B"))
+    pythag(A,B)
 def pythag(A,B):
 #TO DO  
-
+    C = math.sqrt(A**2+B**2)
+    print("hyp = ",C)
 main()
